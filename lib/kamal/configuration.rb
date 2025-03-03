41643b49fe84f6b88173702f729087faafb5fe93
Pavel Guseynov <pavel@didlogic.com>
2025-03-03 20:00:35 +0500

    Use custom docker image with kamal-proxy v0.8.5-dev with path-based routing support

--- lib/kamal/configuration.rb
+++ lib/kamal/configuration.rb
@@ -14,7 +14,7 @@ class Kamal::Configuration
 
   include Validation
 
-  PROXY_MINIMUM_VERSION = "v0.8.4"
+  PROXY_MINIMUM_VERSION = "v0.8.6"
   PROXY_HTTP_PORT = 80
   PROXY_HTTPS_PORT = 443
   PROXY_LOG_MAX_SIZE = "10m"
@@ -262,7 +262,7 @@ class Kamal::Configuration
   end
 
   def proxy_image
-    "basecamp/kamal-proxy:#{PROXY_MINIMUM_VERSION}"
+    "pavelguseynov/kamal-proxy:#{PROXY_MINIMUM_VERSION}"
   end
 
   def proxy_container_name
